a4da2fe19f4b19ac32d6dc7bda563fc3062703e1
Matthew <mshin1995@gmail.com>
2020-03-03 20:07:38 -0800

    start calendar component

---
+++ src/components/Calendar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import dateFns from 'date-fns'
+
+class Calendar extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Calendar</h2>
+            </div>
+        );
+    }
+}
+
+export default Calendar
